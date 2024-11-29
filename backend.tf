e22478b4a7d13131b704704bf62ba1c21e7ac10a
Mir <bachchu333@gmail.com>
2024-11-29 21:47:11 +0530

    gather.tf deleted and ami edited

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mir-terraform-s3-bucket"
-    key    = "key/terraform.tfstate"
+    key    = "jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
 }
--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami                    = data.aws_ami.ami.image_id
+  ami                    = "ami-09b0a86a2c84101e1"
   instance_type          = "t2.2xlarge"
   key_name               = var.key-name
   subnet_id              = aws_subnet.public-subnet.id
