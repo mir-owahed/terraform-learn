7a02ea33522b2ce3d72db40e7c97395542408886
Mir Owahed Ali <133110800+mir-owahed@users.noreply.github.com>
2024-12-02 14:56:40 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "mir-terraform-s3-bucket"
     key    = "jenkins/terraform.tfstate"
     dynamodb_table = "dynamodb-state-locking"
-    region = "ap-south-1"
-  }
+    encrypt        = true
+}
 }
