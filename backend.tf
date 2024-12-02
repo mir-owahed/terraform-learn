00e395e4d30cd27a60ff906d81c68aa20a12fb93
Mir Owahed Ali <133110800+mir-owahed@users.noreply.github.com>
2024-12-02 15:07:15 +0530

    region added in backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "mir-terraform-s3-bucket"
     key    = "jenkins/terraform.tfstate"
     dynamodb_table = "dynamodb-state-locking"
+    region         = "us-east-1"
     encrypt        = true
 }
 }
