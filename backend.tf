a8192d566a37973e04ba93cd70bacdc02fe8827c
Mir Owahed Ali <133110800+mir-owahed@users.noreply.github.com>
2024-12-02 14:54:47 +0530

    dynamo table added.

--- backend.tf
+++ backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "mir-terraform-s3-bucket"
     key    = "jenkins/terraform.tfstate"
+    dynamodb_table = "dynamodb-state-locking"
     region = "ap-south-1"
   }
 }
